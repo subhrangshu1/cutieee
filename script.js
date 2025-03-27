f9ca642525253cb9611c4776389aa9d298f42598
Subhrangshu <163316574+subhrangshu1@users.noreply.github.com>
2025-03-27 15:52:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,29 +4,19 @@ var rotateSpeed = -60; // unit: seconds/360 degrees
 var imgWidth = 120; // width of images (unit: px)
 var imgHeight = 170; // height of images (unit: px)
 
-// Link of background music - set 'null' if you dont want to play background music
-var bgMusicURL = 'Rashid Ali - Kabhi Kabhi Aditi (lyrics)-[AudioTrimmer.com].mp3';
-var bgMusicControls = true; // Show UI music control
+// Link of background music - set 'null' if you don't want music
+var bgMusicURL = 'Kabhi kabhi.mp3'; 
 
-// Create audio element dynamically
-var audio = document.createElement('audio');
-audio.src = bgMusicURL;
-audio.loop = true; // Loop the music
-audio.autoplay = true; // Try to autoplay
-if (bgMusicControls) audio.controls = true; // Show controls if needed
-
-// Append audio to the page
-document.getElementById('music-container').appendChild(audio);
-
-// Handle autoplay restriction
-document.addEventListener('click', function() {
-    audio.play().catch(error => console.log('Autoplay blocked:', error));
-}, { once: true }); // Ensures it runs only once
-
-
+// Create and autoplay the audio
+var audio = new Audio(bgMusicURL);
+audio.loop = true;
+audio.play().catch(() => {
+    document.addEventListener('click', function() {
+        audio.play();
+    }, { once: true });
+});
 
 // ===================== start =======================
-// animation start after 1000 miliseconds
 setTimeout(init, 1000);
 
 var odrag = document.getElementById('drag-container');
@@ -35,11 +25,9 @@ var aImg = ospin.getElementsByTagName('img');
 var aVid = ospin.getElementsByTagName('video');
 var aEle = [...aImg, ...aVid]; // combine 2 arrays
 
-// Size of images
 ospin.style.width = imgWidth + "px";
 ospin.style.height = imgHeight + "px";
 
-// Size of ground - depend on radius
 var ground = document.getElementById('ground');
 ground.style.width = radius * 3 + "px";
 ground.style.height = radius * 3 + "px";
@@ -53,49 +41,30 @@ function init(delayTime) {
 }
 
 function applyTranform(obj) {
-  // Constrain the angle of camera (between 0 and 180)
   if(tY > 180) tY = 180;
   if(tY < 0) tY = 0;
-
-  // Apply the angle
   obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
 }
 
 function playSpin(yes) {
-  ospin.style.animationPlayState = (yes?'running':'paused');
+  ospin.style.animationPlayState = (yes ? 'running' : 'paused');
 }
 
-var sX, sY, nX, nY, desX = 0,
-    desY = 0,
-    tX = 0,
-    tY = 10;
+var sX, sY, nX, nY, desX = 0, desY = 0, tX = 0, tY = 10;
 
-// auto spin
 if (autoRotate) {
   var animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
   ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
 }
 
-// add background music
-if (bgMusicURL) {
-  document.getElementById('music-container').innerHTML += `
-<audio src="${bgMusicURL}" ${bgMusicControls? 'controls': ''} autoplay loop>
-<p>If you are reading this, it is because your browser does not support the audio element.</p>
-</audio>
-`;
-}
-
-// setup events
 document.onpointerdown = function (e) {
   clearInterval(odrag.timer);
   e = e || window.event;
-  var sX = e.clientX,
-      sY = e.clientY;
+  var sX = e.clientX, sY = e.clientY;
 
   this.onpointermove = function (e) {
     e = e || window.event;
-    var nX = e.clientX,
-        nY = e.clientY;
+    var nX = e.clientX, nY = e.clientY;
     desX = nX - sX;
     desY = nY - sY;
     tX += desX * 0.1;
@@ -120,7 +89,6 @@ document.onpointerdown = function (e) {
     }, 17);
     this.onpointermove = this.onpointerup = null;
   };
-
   return false;
 };
 
