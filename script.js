dc23e70e330f57b3d5e77aa004aa8db5190d65f6
Subhrangshu <163316574+subhrangshu1@users.noreply.github.com>
2025-03-27 23:42:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ var rotateSpeed = -60; // unit: seconds/360 degrees
 var imgWidth = 120; // width of images (unit: px)
 var imgHeight = 170; // height of images (unit: px)
 
-// Link of background music - set 'null' if you don't want to play background music
+// Link of background music - set 'null' if you dont want to play background music
 var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
 var bgMusicControls = true; // Show UI music control
 
+
+
 // ===================== start =======================
+// animation start after 1000 miliseconds
 setTimeout(init, 1000);
 
 var odrag = document.getElementById('drag-container');
@@ -17,14 +20,15 @@ var aImg = ospin.getElementsByTagName('img');
 var aVid = ospin.getElementsByTagName('video');
 var aEle = [...aImg, ...aVid]; // combine 2 arrays
 
+// Size of images
 ospin.style.width = imgWidth + "px";
 ospin.style.height = imgHeight + "px";
 
+// Size of ground - depend on radius
 var ground = document.getElementById('ground');
 ground.style.width = radius * 3 + "px";
 ground.style.height = radius * 3 + "px";
 
-// Initialize the positions of the images
 function init(delayTime) {
   for (var i = 0; i < aEle.length; i++) {
     aEle[i].style.transform = "rotateY(" + (i * (360 / aEle.length)) + "deg) translateZ(" + radius + "px)";
@@ -33,43 +37,50 @@ function init(delayTime) {
   }
 }
 
-// Apply transformation to the container
 function applyTranform(obj) {
-  if (tY > 180) tY = 180;
-  if (tY < 0) tY = 0;
+  // Constrain the angle of camera (between 0 and 180)
+  if(tY > 180) tY = 180;
+  if(tY < 0) tY = 0;
 
+  // Apply the angle
   obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
 }
 
 function playSpin(yes) {
-  ospin.style.animationPlayState = (yes ? 'running' : 'paused');
+  ospin.style.animationPlayState = (yes?'running':'paused');
 }
 
-var sX, sY, nX, nY, desX = 0, desY = 0, tX = 0, tY = 10;
+var sX, sY, nX, nY, desX = 0,
+    desY = 0,
+    tX = 0,
+    tY = 10;
 
-// Auto rotate the carousel
+// auto spin
 if (autoRotate) {
   var animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
   ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
 }
 
+// add background music
 if (bgMusicURL) {
   document.getElementById('music-container').innerHTML += `
-    <audio src="${bgMusicURL}" ${bgMusicControls ? 'controls' : ''} autoplay loop>
-      <p>If you are reading this, it is because your browser does not support the audio element.</p>
-    </audio>
-  `;
+<audio src="${bgMusicURL}" ${bgMusicControls? 'controls': ''} autoplay loop>
+<p>If you are reading this, it is because your browser does not support the audio element.</p>
+</audio>
+`;
 }
 
-// Event to track mouse or touch movement
+// setup events
 document.onpointerdown = function (e) {
   clearInterval(odrag.timer);
   e = e || window.event;
-  var sX = e.clientX, sY = e.clientY;
+  var sX = e.clientX,
+      sY = e.clientY;
 
   this.onpointermove = function (e) {
     e = e || window.event;
-    var nX = e.clientX, nY = e.clientY;
+    var nX = e.clientX,
+        nY = e.clientY;
     desX = nX - sX;
     desY = nY - sY;
     tX += desX * 0.1;
@@ -79,7 +90,7 @@ document.onpointerdown = function (e) {
     sY = nY;
   };
 
-  this.onpointerup = function () {
+  this.onpointerup = function (e) {
     odrag.timer = setInterval(function () {
       desX *= 0.95;
       desY *= 0.95;
@@ -98,44 +109,6 @@ document.onpointerdown = function (e) {
   return false;
 };
 
-// Add touch support for mobile devices
-document.ontouchstart = function(e) {
-  clearInterval(odrag.timer);
-  e = e || window.event;
-  var sX = e.touches[0].clientX, sY = e.touches[0].clientY;
-
-  this.ontouchmove = function(e) {
-    e = e || window.event;
-    var nX = e.touches[0].clientX, nY = e.touches[0].clientY;
-    desX = nX - sX;
-    desY = nY - sY;
-    tX += desX * 0.1;
-    tY += desY * 0.1;
-    applyTranform(odrag);
-    sX = nX;
-    sY = nY;
-  };
-
-  this.ontouchend = function() {
-    odrag.timer = setInterval(function () {
-      desX *= 0.95;
-      desY *= 0.95;
-      tX += desX * 0.1;
-      tY += desY * 0.1;
-      applyTranform(odrag);
-      playSpin(false);
-      if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
-        clearInterval(odrag.timer);
-        playSpin(true);
-      }
-    }, 17);
-    this.ontouchmove = this.ontouchend = null;
-  };
-
-  return false;
-};
-
-// Mouse wheel zoom functionality (optional)
 document.onmousewheel = function(e) {
   e = e || window.event;
   var d = e.wheelDelta / 20 || -e.detail;
