c74a9deb8016b14be1dcdd8cd7fe948d779483c2
Subhrangshu <163316574+subhrangshu1@users.noreply.github.com>
2025-03-28 00:18:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ var imgWidth = 120; // width of images (unit: px)
 var imgHeight = 170; // height of images (unit: px)
 
 // Link of background music - set 'null' if you dont want to play background music
-var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
+var bgMusicURL = 'path/to/KabhiKabhi.mp3'; // Replace with the correct path to your file
 var bgMusicControls = true; // Show UI music control
+
+// add background music
+if (bgMusicURL) {
+  document.getElementById('music-container').innerHTML += `
+    <audio src="${bgMusicURL}" ${bgMusicControls ? 'controls' : ''} autoplay loop>
+      <p>If you are reading this, it is because your browser does not support the audio element.</p>
+    </audio>
+  `;
+}
 
 
 
