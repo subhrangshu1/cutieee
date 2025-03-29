945547ddbee225e28ce74e8ac3cb8329f4b4cd34
Subhrangshu <163316574+subhrangshu1@users.noreply.github.com>
2025-03-29 17:17:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var radius = 240; // how big of the radius
+var radius = 300; // how big of the radius
 var autoRotate = true; // auto rotate or not
 var rotateSpeed = -60; // unit: seconds/360 degrees
-var imgWidth = 120; // width of images (unit: px)
-var imgHeight = 170; // height of images (unit: px)
+var imgWidth = 150; // width of images (unit: px)
+var imgHeight = 200; // height of images (unit: px)
 
 // Remove background music container
 document.getElementById('music-container').remove();
